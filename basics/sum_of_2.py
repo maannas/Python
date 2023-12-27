beb41dfe6b57a7e9c20fc705968fdf8779448502
Manas Kolaskar <148738351+maannas@users.noreply.github.com>
2023-12-27 16:39:34 +0000

    commit

---
+++ basics/sum_of_2.py
@@ -0,0 +1,7 @@
+# get input
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+#store the input in sum
+sum_of_numbers = num1 + num2
+#print the result
+print(f"The sum of {num1} and {num2} is {sum_of_numbers}")
--- variable.py
+++
@@ -1,6 +0,0 @@
-name = "Manas"
-roll_number = 36
-percentage = 85.3
-
-print("My name is " + name + " my roll number is ",roll_number)
-print("my percenatge is ",percentage)
