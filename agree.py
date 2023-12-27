d9110156348b073ae5f08323a51ae5947fbcc5d7
Manas Kolaskar <148738351+maannas@users.noreply.github.com>
2023-12-27 06:33:41 +0000

    commit

--- agree.py
+++
@@ -1,6 +0,0 @@
-s = input("Do you agree? ")
-
-if s in ["y","Y"]:
-    print("Agreed.")
-elif s in ["n","N"]:
-    print("Not Agreed.")
---
+++ variable.py
@@ -0,0 +1,6 @@
+name = "Manas"
+roll_number = 36
+percentage = 85.3
+
+print("My name is " + name + " my roll number is ",roll_number)
+print("my percenatge is ",percentage)
